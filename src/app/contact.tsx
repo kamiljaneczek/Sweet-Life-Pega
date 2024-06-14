795920b1b2182759cb11218af59d9a9b798c6b18
kamiljaneczek <janeczekkamil@gmail.com>
2024-06-14 16:30:51 +0200

    dx api call for contact form

--- src/app/contact.tsx
+++ src/app/contact.tsx
@@ -5,35 +5,58 @@ import { Textarea } from '../design-system/ui/textarea';
 import Footer from './components/footer';
 import { Button } from '../design-system/ui/button';
 import useConstellation from '../hooks/useConstellation';
-import { useEffect, useState } from 'react';
-import { getSdkConfig } from '@pega/auth/lib/sdk-auth-manager';
+import { useState } from 'react';
+
 import Loading from './components/loading';
-import classNames from 'classnames';
 
 const Contact = () => {
-  const [showPega, setShowPega] = useState('Info'); // Info, Pega, Confirmation
+  const [caseID, setCaseID] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
   const isPegaReady = useConstellation();
 
-  useEffect(() => {
-    if (isPegaReady) {
-      getSdkConfig().then(sdkConfig => {
-        let mashupCaseType = sdkConfig.serverConfig.appMashupCaseType;
-        if (!mashupCaseType) {
-          const caseTypes = PCore.getEnvironmentInfo().environmentInfoObject.pyCaseTypeList;
-          mashupCaseType = caseTypes[2].pyWorkTypeImplementationClassName;
-        }
-
-        const options: any = {
-          pageName: 'pyEmbedAssignment',
-          startingFields: {}
-        };
-        (PCore.getMashupApi().createCase(mashupCaseType, PCore.getConstants().APP.APP, options) as any).then(() => {
-          console.log('createCase rendering is complete');
-        });
+  function createInquiryCase() {
+    let token = '';
+    (async () => {
+      const responseToken = await fetch('https://lab0339.lab.pega.com/prweb/PRRestService/oauth2/v1/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: `custom-bearer`
+        },
+        body: 'client_id=11351708984521870813&grant_type=custom-bearer&enable_psyncId=true'
       });
-      setShowPega('Pega');
-    }
-  }, [isPegaReady]);
+      const dataToken = await responseToken.json();
+
+      console.log('token: ', dataToken.access_token);
+      token = dataToken.access_token;
+
+      const caseBody = {
+        caseTypeID: 'SL-TellUsMoreRef-Work-Inquiry',
+        parentCaseID: '',
+        content: {
+          FirstName: 'Adam',
+          LastName: 'Smith',
+          Email: email,
+          InquiryMessage: message
+        }
+      };
+
+      const responseCase = await fetch('https://lab0339.lab.pega.com/prweb/api/application/v2/cases?viewType=none&pageName=', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(caseBody)
+      });
+      const dataCase = await responseCase.json();
+      setCaseID(dataCase.data.caseInfo.content.pyID);
+
+      console.log('dataCase: ', dataCase);
+    })();
+  }
 
   return (
     <>
@@ -46,43 +69,70 @@ const Contact = () => {
               <p className='mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl'>
                 Want to send feedback about a beta feature? Need details about our Business plan? Let us know.
               </p>
-              <form action='#' className='space-y-8'>
-                <div>
-                  <Input type='email' id='email' label='Your email' helperText='' placeholder='name@pega.com' InputProps='' error={false} required />
-                </div>
-                <div>
-                  <Input
-                    type='text'
-                    id='subject'
-                    label='Subject'
-                    helperText=''
-                    InputProps=''
-                    error={false}
-                    className='block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
-                    placeholder='Let us know how we can help you'
-                    required
-                  />
-                </div>
-                <div className='sm:col-span-2'>
-                  <Textarea
-                    id='message'
-                    rows={6}
-                    helperText=''
-                    size='medium'
-                    label='Your message'
-                    variant='outlined'
-                    className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
-                    placeholder='Leave a comment...'
-                  />
-                </div>
-                <Button type='submit' variant='default'>
-                  Send message
-                </Button>
-              </form>
+              {caseID == '' ? (
+                <form
+                  onSubmit={e => {
+                    e.preventDefault();
+                    createInquiryCase();
+                  }}
+                  className='space-y-8'
+                >
+                  <div>
+                    <Input
+                      type='email'
+                      id='email'
+                      label='Your email'
+                      helperText=''
+                      value={email}
+                      onChange={e => {
+                        setEmail(e.target.value);
+                      }}
+                      placeholder='name@pega.com'
+                      InputProps=''
+                      error={false}
+                      required
+                    />
+                  </div>
+                  <div>
+                    <Input
+                      type='text'
+                      id='subject'
+                      label='Subject'
+                      helperText=''
+                      InputProps=''
+                      value={subject}
+                      onChange={e => setSubject(e.target.value)}
+                      error={false}
+                      className='block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
+                      placeholder='Let us know how we can help you'
+                      required
+                    />
+                  </div>
+                  <div className='sm:col-span-2'>
+                    <Textarea
+                      id='message'
+                      rows={6}
+                      helperText=''
+                      size='medium'
+                      value={message}
+                      onChange={e => setMessage(e.target.value)}
+                      label='Your message'
+                      variant='outlined'
+                      className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
+                      placeholder='Leave a comment...'
+                    />
+                  </div>
+                  <Button type='submit' variant='default'>
+                    Send message
+                  </Button>
+                </form>
+              ) : (
+                <div className='flex flex-row align-middle items-center justify-center'>Case created successfully: {caseID} </div>
+              )}
             </div>
-          </section>{' '}
+          </section>
           <div className='flex flex-row align-middle items-center justify-center'>
-            <div id='pega-root' className={classNames('flex-grow w-full max-w-3xl', { hidden: showPega !== 'Pega' })} />
+            <div id='pega-root' />
           </div>
         </div>
       ) : (
