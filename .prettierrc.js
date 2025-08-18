f01547d1f76ab23c9afc104dc80497ca3fbbb5ac
tumms2021389 <97188066+tumms2021389@users.noreply.github.com>
2025-08-18 19:45:01 +0530

    fix: update dependencies in package.json (#572)
    * fix: update dependencies in package.json
    
    * chore: update dependencies and migrate to React 18 and MUI6

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,7 @@
-const pegaPrettierConfig = require('@pega/prettier-config');
+// Prettier config options: https://prettier.io/docs/en/options.html
+// Shared front-end config: https://git.pega.io/projects/FE/repos/configs/browse/packages/prettier-config/index.json
 
-module.exports = {
-  ...pegaPrettierConfig,
+module.exports = import('@pega/prettier-config').then(pegaPrettierConfig => ({
+  ...pegaPrettierConfig.default,
   printWidth: 150
-};
+}));
